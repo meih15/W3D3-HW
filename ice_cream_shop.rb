1446ff2cfef2bcdba59b0618d96d60911eb76b18
meihuang <meihuang8328@gmail.com>
2022-11-22 22:18:26 -0500

    exercise4: ice_cream_shop

---
+++ ice_cream_shop.rb
@@ -0,0 +1,20 @@
+def ice_cream_shop(flavors, favorite)
+
+    return false if flavors == []
+
+    if flavors[0] == favorite
+        return true
+    else
+        ice_cream_shop(flavors[1..-1], favorite)
+    end
+
+
+
+end
+
+
+p ice_cream_shop(['vanilla', 'strawberry'], 'blue moon')  # => returns false
+p ice_cream_shop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea')  # => returns true
+p ice_cream_shop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio')  # => returns false
+p ice_cream_shop(['moose tracks'], 'moose tracks')  # => returns true
+p ice_cream_shop([], 'honey lavender')  # => returns false
